7e37004645d3a687dc1a1cf18cc284513167edd2
JS <weldhagenjustin@gmail.com>
2026-06-11 18:56:35 +0200

    Created Parts component
    Just added styling and made the visual part of the component.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,6 +4,7 @@ import { CustomersPanel } from '../features/customers/components/CustomersPanel'
 import { VehiclesPanel } from '../features/vehicles/components/VehiclesPanel';
 import { JobsPanel } from '../features/jobs/components/JobsPanel';
 import { QuotesPanel } from '../features/quotes/components/QuotesPanel';
+import { PartsPanel } from '../features/parts/components/PartsPanel';
 
 export default function HomePage() {
   return (
@@ -15,6 +16,7 @@ export default function HomePage() {
         <VehiclesPanel />
         <JobsPanel />
         <QuotesPanel />
+        <PartsPanel/>
       </div>
     </AppShell>
   );
