a800c03b9d7501f383f70909c78fa6edb11e5d66
JS <weldhagenjustin@gmail.com>
2026-06-15 18:33:53 +0200

    Added the POST Endpoint
    Still in progress

--- apps/web/src/app/api/customers/route.ts
+++ apps/web/src/app/api/customers/route.ts
@@ -1,3 +1,6 @@
+import { createCustomerSchema } from '@/features/customers/schemas/customer.schema';
+import { createCustomer } from '@/features/customers/services/customer.service';
+import { requireTenantContext } from '@/lib/tenancy/tenant-context';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
@@ -17,4 +20,18 @@ export async function GET(request: Request) {
 };
 
 // TODO: Query the database for active or non-archived
-//  Add the catch statement to the try operator dont forget to return JSON
+// Add the catch statement to the try operator dont forget to return JSON
+// Add the POST method to create a new customer and return the created customer in the response
+
+export async function POST(req: Request) {
+  try {
+    const tenant = await requireTenantContext();
+    const body = await req.json();
+    const input = createCustomerSchema.parse(body);
+    const customer = await createCustomer(tenant.tenantId, input);
+
+    return NextResponse.json(customer, { status: 201 });
+  }
+};
+
+//TODO: Add the catch statement to the try operator dont forget to return JSON
