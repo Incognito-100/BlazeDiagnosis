901e2557fc99d4cfca0c44d4f432497781f12f83
Izak Van Heerden <izakvanheerden1@gmail.com>
2026-06-24 14:59:25 +0200

    added customer details page and ability to edit

--- apps/web/src/app/[locale]/(station)/station/customers/[id]/page.tsx
+++ apps/web/src/app/[locale]/(station)/station/customers/[id]/page.tsx
@@ -1,1 +1,11 @@
 import { CustomerDetail } from "@/components/customers/customerDetail";
+
+export default function Page({ params }: { params: { id: string } }) {
+    const customerId = params?.id ?? '1';
+
+    return (
+        <>
+            <CustomerDetail customerId={customerId} />
+        </>
+    );
+}
